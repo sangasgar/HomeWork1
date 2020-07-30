83ed53f0939add06f2563a2647dd953f8c03f6de
Garegin <sangas@yandex.ru>
2020-07-30 15:24:14 +0300

    Домашнее задание сделано с 1 по 3

--- src/Main.java
+++ src/Main.java
@@ -9,33 +9,209 @@ public class Main {
      * 3. * Попробовать переписать логику проверки победы, чтобы она работала для поля 5х5 и количества фишек 4. Очень желательно не делать это просто набором условий для каждой из возможных ситуаций;
      * 4. *** Доработать искусственный интеллект, чтобы он мог блокировать ходы игрока.
      */
-    static  final int SIZE = 3;
+    static  final int SIZE = 5;
     static final char DOT_X = 'X';
     static final char DOT_O = 'O';
     static final char DOT_EMPTY = '.';
     static char[][] map;
-    static Scanner scanner = new Scanner(System.in);
+    static Scanner sc = new Scanner(System.in);
     static Random random = new Random();
+
+    public static void main(String[] args) {
+        initMap();
+        printMap();
+
+      while (true) {
+          System.out.println("Введите первое значение ячеек");
+          humanTurn();
+          printMap();
+          if(checkWin(DOT_X)) {
+              System.out.println("Ты победил");
+              break;
+          }
+          if(isFull()) {
+              System.out.println("Ничья");
+              break;
+          }
+          System.out.println("Введите второе значение ячеек");
+          humanTurn();
+          printMap();
+          if(checkWin(DOT_X)) {
+              System.out.println("Ты победил");
+              break;
+          }
+          if(isFull()) {
+              System.out.println("Ничья");
+              break;
+          }
+          aiTurn();
+          printMap();
+          if(checkWin(DOT_O)) {
+              System.out.println("Компьютер победил");
+              break;
+          }
+          if(isFull()) {
+              System.out.println("Ничья");
+              break;
+          }
+          aiTurn();
+          printMap();
+          if(checkWin(DOT_O)) {
+              System.out.println("Компьютер победил");
+              break;
+          }
+          if(isFull()) {
+              System.out.println("Ничья");
+              break;
+          }
+      }
+
+    }
     public static void initMap() {
         map = new char[SIZE][SIZE];
         for (int i =0; i < SIZE; i++) {
             for (int j=0; j <SIZE; j++) {
-
+                map[i][j] = DOT_EMPTY;
             }
         }
     }
     public static void printMap() {
-        for (int i = 0; i <SIZE;i++){
+        System.out.print("  ");
+        for (int i = 0; i < SIZE; i++) {
+            System.out.print(i + 1 + " ");
+        }
+        System.out.println();
+        for (int i = 0; i <SIZE; i++){
+            System.out.print(i + 1 + " ");
+            for (int j = 0; j < SIZE; j ++) {
+                System.out.print(map[i][j] + " ");
+            }
             System.out.println();
-
         }
     }
-    public static void main(String[] args) {
-
-
-
-
+    public static void humanTurn() {
+        int x,y;
+        do {
+            System.out.println("Input X, Y");
+            x = sc.nextInt() - 1;
+            y = sc.nextInt() - 1;
+        } while (!isCelValid(y,x));
+        map[y][x] = DOT_X;
     }
 
+    public static boolean isCelValid(int y, int x) {
+        if (x<0 || y<0 || x >=SIZE || y>=SIZE) {
+            return false;
+        }
+        return map[y][x] == DOT_EMPTY;
+    }
+    public static void aiTurn() {
+        int x,y;
+        do {
+            x = random.nextInt(SIZE);
+            y = random.nextInt(SIZE);
+        } while (!isCelValid(y,x));
+        map[y][x] = DOT_O;
+    }
 
+    public static boolean isFull() {
+        for (int i = 0; i < SIZE; i++) {
+            for (int j = 0; j < SIZE; j++) {
+                if(map[i][j] == DOT_EMPTY) {
+                    return false;
+                }
+            }
+
+        }
+        return true;
+    }
+    public static boolean checkWin(char c) {
+
+        int score = 0;
+        for (int i = 0; i < SIZE; i++) {
+            if (map[0][i] == c) {
+            score += 1;
+            }
+        }
+        if (score == SIZE) {
+            return true;
+        } else {
+            score = 0;
+        }
+
+        for (int i = 0; i < SIZE; i++) {
+            if (map[1][i] == c) {
+                score += 1;
+            }
+        }
+        if (score == SIZE) {
+            return true;
+        } else {
+            score = 0;
+        }
+        for (int i = 0; i < SIZE; i++) {
+            if (map[2][i] == c) {
+                score += 1;
+            }
+        }
+
+
+        if (map[0][0] == c && map[1][0] == c && map[2][0] == c) {return true;}
+        if (map[0][1] == c && map[1][1] == c && map[2][1] == c) {return true;}
+        if (map[0][2] == c && map[1][2] == c && map[2][2] == c) {return true;}
+        for (int i = 0; i < SIZE; i++) {
+            if (map[i][0] == c) {
+                score += 1;
+            }
+        }
+        if (score == SIZE) {
+            return true;
+        } else {
+            score = 0;
+        }
+
+        for (int i = 0; i < SIZE; i++) {
+            if (map[i][1] == c) {
+                score += 1;
+            }
+        }
+        if (score == SIZE) {
+            return true;
+        } else {
+            score = 0;
+        }
+        for (int i = 0; i < SIZE; i++) {
+            if (map[i][2]== c) {
+                score += 1;
+            }
+        }
+        if (score == SIZE) {
+            return true;
+        } else {
+            score = 0;
+        }
+
+        for (int i = 0; i < SIZE; i++) {
+            if (map[i][i]== c) {
+                score += 1;
+            }
+        }
+        if (score == SIZE) {
+            return true;
+        } else {
+            score = 0;
+        }
+        for (int i = 0, j = SIZE-1; i < SIZE && j < SIZE; i++, j --) {
+            if (map[i][j]== c) {
+                score += 1;
+            }
+        }
+        if (score == SIZE) {
+            return true;
+        } else {
+            score = 0;
+        }
+        if (map[0][2] == c && map[1][1] == c && map[2][0] == c) {return true;}
+       return false;
+    }
 }
