6b6a5dcff6d8b40edf1b3640faa4f571a4150c57
Garegin <sangas@yandex.ru>
2020-08-01 00:54:40 +0300

    ООП Домашнее задание сделано все

--- src/Employeers.java
+++ src/Employeers.java
@@ -1,16 +1,33 @@
 public class Employeers {
-    String name;
-    int age;
+    String fullName;
+    String position;
+    String email;
+    String phone;
     String salary;
+    int age;
 
-
-    public Employeers(String _name, int _age, String _salary) {
-        this.name = _name;
-        this.age = _age;
-        this.salary = _salary;
+    public Employeers(String fullName, String position, String email, String phone, String salary, int age) {
+        this.fullName = fullName;
+        this.position = position;
+        this.email = email;
+        this.phone = phone;
+        this.salary = salary;
+        this.age = age;
     }
 
-    public void info () {
-        System.out.printf("Name %s, Salary %s, Age %d", name, salary, age);
-    };
+    public void  info () {
+        System.out.printf("FullName %s\nposition %s \nemail %s \nphone %s \nsalary %s \nage %d", fullName, position, email, phone, salary, age);
+        System.out.println();
+        System.out.println();
+    }
+
+    public void infoAge(int ageUser) {
+        if (age > ageUser) {
+            System.out.printf("FullName %s\nposition %s \nemail %s \nphone %s \nsalary %s \nage %d", fullName, position, email, phone, salary, age);
+            System.out.println();
+            System.out.println();
+        }
+
+    }
+
 }
--- src/Main.java
+++ src/Main.java
@@ -4,8 +4,19 @@ import java.util.Scanner;
 
 public class Main {
     public static void main(String[] args) {
-    Employeers emp1 = new Employeers("Garegin",18, "25000" );
+    Employeers emp = new Employeers("Garegin","It director","sangas@yandex.ru","89261119990","250000",32);
 
-    emp1.info();
+    emp.info();
+        Employeers[] persArray = new Employeers[5];
+        persArray[0] = new Employeers("Garegin","It director","sangas@yandex.ru","89166897511","250000",32);
+        persArray[1] = new Employeers("Николай","It director","home@yandex.ru","89055633449","300000",60);
+        persArray[2] = new Employeers("Валера","It director","ema@yandex.ru","89261119990","40000",25);
+        persArray[3] = new Employeers("Георгий","It director","lam@gmail.com","89882552222","70000",45);
+        persArray[4] = new Employeers("Грач","It director","samara@yandex.ru","89997774411","100000",50);
+
+        for (int i = 0; i < persArray.length; i++) {
+            persArray[i].infoAge(40);
+        }
+
     }
 }
